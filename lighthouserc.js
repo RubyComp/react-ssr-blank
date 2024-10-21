9313c9261e61588556747050a172b8936f683af7
Alex Seleznev <alex61225@gmail.com>
2024-10-21 05:40:21 +0300

    doc, cleanup

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,19 +1,20 @@
 module.exports = {
 	ci: {
 		collect: {
-			url: ['http://localhost:3000', 'http://localhost:3000/test'], // Укажите страницы для тестирования
-			numberOfRuns: 3, // Количество прогонов для средней метрики
-			startServerCommand: 'npm run start', // Команда для запуска вашего сервера
-			startServerReadyPattern: 'ready - started server', // Паттерн для ожидания готовности сервера
-			port: 3000, // Явно указываем порт 3000
+			url: ['http://localhost:3000', 'http://localhost:3000/test'],
+			numberOfRuns: 2,
+			startServerCommand: 'npm run start',
+			startServerReadyPattern: 'ready - started server',
+			port: 3000,
 		},
 		assert: {
 			assertions: {
-				'categories:performance': ['error', { minScore: 0.9 }], // Устанавливаем критерий для оценки производительности
+				'categories:performance': ['error', { minScore: 0.9 }],
 			},
 		},
 		upload: {
-			target: 'temporary-public-storage', // Опция для хранения отчётов (можно настроить иначе)
+			target: 'filesystem',
+			outputDir: './.lighthouseci',
 		},
 	},
 };
